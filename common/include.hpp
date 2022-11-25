a08cce2e6e77cd9157c692578577a610398e1fbc
Sala2Code <109032171+Sala2Code@users.noreply.github.com>
2022-11-25 23:02:37 +0100

    Add files via upload

---
+++ common/include.hpp
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_HPP
+#define INCLUDE_HPP
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include "../FILE/include/glad/glad.h"
+#include "../FILE/include/GLFW/glfw3.h"
+#include "../FILE/include/glm/glm.hpp"
+#include "../FILE/include/glm/gtx/transform.hpp"
+#include "../FILE/include/stb/stb_image.h"
+#include <vector>
+
+#endif
